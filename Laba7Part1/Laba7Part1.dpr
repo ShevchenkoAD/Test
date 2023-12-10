b01ae35a8a4433fe02bc5714143b7348fa0a6cf5
ShevchenkoAD <sasha42612@gmail.com>
2023-12-10 15:40:11 +0300

    Revert "All tests are completed"
    This reverts commit 261f96fabff6d9a9f3ff2048c732b96795b16314.

--- Laba7Part1/Laba7Part1.dpr
+++ Laba7Part1/Laba7Part1.dpr
@@ -241,6 +241,4 @@ end.
 //
 //I wanted to send an email update to you let you know how things have been going during my semester abroad here in Málaga, Spain. I've already been here for six weeks, and I feel like I am finally adapting to the culture. I'm also speaking the language more fluently.
 //
-//  lebb ellb thmon   haha house bell lecture ouesh  haha lleb ha     month my may mi ma haha hsoue  hah  haha
-// All Tests are comleted
-
+//  lebb ellb thmon   haha house bell lecture ouesh  haha lleb ha     month my may mi ma haha hsoue  hah  haha      
